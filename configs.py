40268b67c13cf72b22638a5c284cdbba1a25a374
Omar Younis <omar.younis98@gmail.com>
2023-01-05 12:12:17 +0100

    add new configs

--- configs.py
+++ configs.py
@@ -1,4 +1,4 @@
-config_kbest_transf = dict(
+kbest_transf = dict(
     main="simplified_rl_action",
     name="tune_params",
     env_name="breeding_gym:KBestBreedingGym",
@@ -12,7 +12,7 @@ config_kbest_transf = dict(
     sweep_id="f7yelb36"
 )
 
-config_kbest = dict(
+kbest = dict(
     main="simplified_rl_action",
     name="final_launch",
     env_name="breeding_gym:KBestBreedingGym",
@@ -32,18 +32,78 @@ config_kbest = dict(
     n_launches=4,
 )
 
-config_selection = dict(
+selection_cnn = dict(
     main="selection_index_run",
-    name="selection_index",
+    name="selection_final",
     data_size="small",
     group="SelectionIndex",
-    project="BreedingGym"
+    project="BreedingGym",
+    n_envs=32,
+    n_eval_envs=1,
+    #sweep_id="cjel19ew",
+    n_launches=4,
+    gamma=0.92,
+    learning_rate=5e-4,
+    gae_lambda=0.96,
+    n_epochs=7
 )
 
-config_profiling = dict(
-    main="profile",
-    name="profile",
+selection_append_gebv = dict(
+    main="selection_index_run",
+    name="final_launch",
     data_size="small",
-    group="profiling",
-    disable_wandb=True
+    group="SelectionAppendGEBV",
+    project="BreedingGym",
+    n_envs=32,
+    n_eval_envs=1,
+    n_launches=4,
+    #sweep_id="8aswjmox"
+    gamma=0.9434,
+    learning_rate=5.5e-5,
+    gae_lambda=0.9061,
+    n_epochs=8,
+    total_timesteps=1e6
+)
+
+selection_masked_gebv = dict(
+    main="selection_index_run",
+    name="final_launch_1e6",
+    data_size="small",
+    group="SelectionMaskedGEBV",
+    project="BreedingGym",
+    n_envs=32,
+    n_eval_envs=1,
+    n_launches=1,
+    #sweep_id="cd8jybd9"
+    gamma=0.8122,
+    learning_rate=2e-5,
+    gae_lambda=0.9036,
+    n_epochs=5,
+    total_timesteps=1e6
+)
+
+selection_block_mlp = dict(
+    main="selection_index_run",
+    name="tune_params",
+    data_size="small",
+    group="BlockMLP",
+    project="BreedingGym",
+    n_envs=16,
+    n_eval_envs=1,
+    n_launches=8,
+    total_timesteps=3e5,
+    sweep_id="1bn9p5n2"
+)
+
+masked_gebv_cnn = dict(
+    main="selection_index_run",
+    name="tune_params",
+    data_size="small",
+    group="MaskedGEBV_CNN",
+    project="BreedingGym",
+    n_envs=16,
+    n_eval_envs=1,
+    n_launches=8,
+    total_timesteps=3e5,
+    sweep_id="n5l0e7dl"
 )
